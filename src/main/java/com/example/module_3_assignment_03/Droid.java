5e0fe078b90ef1901cc590350fc3fef794088b79
MaximFarmingdale <nikim@farmingdale.edu>
2025-02-24 21:46:08 -0500

    expanded on the car class, adding movement on key press while also modifying the controller and application classes to make it possible.

--- src/main/java/com/example/module_3_assignment_03/Droid.java
+++ src/main/java/com/example/module_3_assignment_03/Droid.java
@@ -5,65 +5,27 @@ import javafx.scene.image.PixelReader;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 
-public class Car {
+public class Droid {
     ImageView imageView;
     double speed;
     boolean in_use;
     String direction;
     ImageView Maze;
 
-    public Car(ImageView imageView, int speed, boolean in_use, ImageView Maze) {
+    public Droid(ImageView imageView, int speed,boolean in_use, ImageView Maze) {
         this.imageView = imageView;
         this.speed = speed;
-        this.in_use = in_use;
         this.direction = "right";
+        this.in_use = in_use;
         this.Maze = Maze;
     }
+
     public void setIn_Use(boolean in_use) {
         this.in_use = in_use;
     }
 
     public boolean getIn_Use() {
         return in_use;
-    }
-
-    public void rotate(String future_direction) {
-        if (future_direction.equals(direction)) {
-            return;
-        }
-        switch (future_direction) {
-            case "right":
-
-                imageView.setScaleX(1);
-                imageView.setScaleY(1);
-                imageView.setRotationAxis(new javafx.geometry.Point3D(0, 0, 1));
-                imageView.setRotate(0);
-                break;
-
-            case "left":
-                // Flip horizontally when facing left
-                imageView.setScaleX(-1);
-                imageView.setScaleY(1);
-                imageView.setRotationAxis(new javafx.geometry.Point3D(0, 0, 1));
-                imageView.setRotate(0);
-                break;
-
-            case "up":
-                imageView.setScaleX(1);
-                imageView.setScaleY(1);
-                imageView.setRotationAxis(new javafx.geometry.Point3D(0, 0, 1));
-                imageView.setRotate(270);
-                break;
-
-            case "down":
-                imageView.setScaleX(1);
-                imageView.setScaleY(-1);
-                imageView.setRotationAxis(new javafx.geometry.Point3D(0, 0, 1));
-                imageView.setRotate(90);
-                break;
-        }
-
-        direction = future_direction;
     }
 
 
@@ -85,18 +47,17 @@ public class Car {
         double relativeX = imageView.getLayoutX() - Maze.getLayoutX();
         double relativeY = imageView.getLayoutY() - Maze.getLayoutY();
 
-        int x = (int) ((relativeX + 50) * scaleX);
-        int y = (int) ((relativeY + 13) * scaleY);
+        int x = (int) ((relativeX + 55) * scaleX);
+        int y = (int) ((relativeY + 10) * scaleY);
         switch (direction) {
             case "right":
                 x += (int) (17 * scaleX);
                 break;
             case "left":
-                x -= (int) (27 * scaleX);
+                x -= (int) (17 * scaleX);
                 break;
             case "up":
                 y -= (int) (6 * scaleY);
-                x -= (int) (7 * scaleX);
                 break;
             case "down":
                 y += (int) (6 * scaleY);
@@ -128,25 +89,25 @@ public class Car {
         //makes sure to also set the direction which we will use to make sure the car is in bounds
         switch (event.getCode()) {
             case W:
-                rotate("up");
+                direction = "up";
                 if (validpath()) {
                     imageView.setLayoutY(imageView.getLayoutY() - speed);
                 }
                 break;
             case S:
-                rotate("down");
+                direction = "down";
                 if (validpath()) {
                     imageView.setLayoutY(imageView.getLayoutY() + speed);
                 }
                 break;
             case A:
-                rotate("left");
+                direction = "left";
                 if (validpath()) {
                     imageView.setLayoutX(imageView.getLayoutX() - speed);
                 }
                 break;
             case D:
-                rotate("right");
+                direction = "right";
                 if (validpath()) {
                     imageView.setLayoutX(imageView.getLayoutX() + speed);
                 }
--- src/main/java/com/example/module_3_assignment_03/HelloController.java
+++ src/main/java/com/example/module_3_assignment_03/HelloController.java
@@ -2,6 +2,7 @@ package com.example.module_3_assignment_03;
 
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.input.KeyCode;
@@ -15,26 +16,51 @@ public class HelloController {
     private ImageView car_one;
 
     @FXML
+    private ImageView droidImgView;
+
+    @FXML
     private ImageView Maze_1;
 
     @FXML
     private ImageView Maze_2;
 
-    @FXML
-    private ImageView Robot;
     // car class
     private Car vehicle;
+
+    private Droid droid;
+
+    @FXML
+    Button ctrlDroid, ctrlCar, animate;
+
     //Initializer for the car, needed to make sure the image is loaded
     public void initialize() {
-        if (car_one != null && Maze_1 != null) {
+        if (car_one != null && Maze_1 != null)
             vehicle = new Car(car_one, 3, true, Maze_1);
-        }
+
+        if (droidImgView != null && Maze_1 != null)
+            droid = new Droid(droidImgView, 3, true, Maze_1);
+
+        ctrlCar.setOnKeyPressed(event -> {
+            droid.setIn_Use(false);
+            vehicle.setIn_Use(true);
+
+        });
+
+        ctrlDroid.setOnKeyPressed(event -> {
+            vehicle.setIn_Use(false);
+            droid.setIn_Use(true);
+        });
+
     }
     //calls the internal move method of the car
     @FXML
     public void user_move(KeyEvent event) {
-        if (vehicle != null) {
+        if (vehicle != null && vehicle.getIn_Use()) {
             vehicle.move(event);
+            return;
+        }
+        if (vehicle != null && droid.getIn_Use()) {
+            droid.move(event);
             return;
         }
     }
@@ -50,4 +76,3 @@ public class HelloController {
     }
 
 }
-
